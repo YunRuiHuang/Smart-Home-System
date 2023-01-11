a23d5294938ce35b96b2155020a09e030dc57ff1
YunRuiHuang <yunruihuang@gmail.com>
2023-01-11 00:49:03 -0500

    update router

--- middleware/project/router/users.js
+++ middleware/project/router/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get("/",(req,res)=>{
-  res.sendFile(path.join(__dirname,'../html/user.html'));
+  res.sendFile(path.join(__dirname,'../views/html/user.html'));
 });
 
 router.get("/:index",(req,res)=>{
