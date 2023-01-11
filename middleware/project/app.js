2977b4333afd5d571d5e96e8776f96cb4aeefd65
YunRuiHuang <yunruihuang@gmail.com>
2023-01-11 00:53:11 -0500

    update router

--- middleware/project/app.js
+++ middleware/project/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
-const routes = require('./router/users.js');
+const routes = require('routers.js');
 const path = require('path');
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{res.sendFile(path.join(__dirname,'/html/home.html'))});
+app.get('/',(req,res)=>{res.sendFile(path.join(__dirname,'views/html/home.html'))});
 app.get('/image/:img',(req,res)=>{
 	let file;
-	const address = "/image/" + req.params.img;
+	const address = "/public/image/" + req.params.img;
 	try{
 	file = path.join(__dirname,address);
 	//res.sendFile(file);
@@ -19,8 +19,8 @@ app.get('/image/:img',(req,res)=>{
 
 });
 
-
-app.use("/user",routes);
+app.use("/dataPort",router.dataPort);
+app.use("/user",routes.user);
 //app.get('/:data',(req,res)=>{res.send(req.params.data)});
 
 app.listen(3000,()=>{console.log("server run at port 3000")});
---
+++ middleware/project/routes.js
@@ -0,0 +1,4 @@
+const users = require('./router/users.js');
+const dataPort = require('./router/dataPort.js');
+
+module.exports={users, dataPort};
