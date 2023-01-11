bf0697de4a3c0681635e3f3ba26037b02888a399
YunRuiHuang <yunruihuang@gmail.com>
2023-01-11 00:50:17 -0500

    update router

--- middleware/project/index.js
+++ middleware/project/index.js
@@ -5,10 +5,10 @@ const path = require('path');
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{res.sendFile(path.join(__dirname,'/html/home.html'))});
+app.get('/',(req,res)=>{res.sendFile(path.join(__dirname,'views/html/home.html'))});
 app.get('/image/:img',(req,res)=>{
 	let file;
-	const address = "/image/" + req.params.img;
+	const address = "public/image/" + req.params.img;
 	try{
 	file = path.join(__dirname,address);
 	//res.sendFile(file);
