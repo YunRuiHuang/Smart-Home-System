82396bc1f995b728ca440f94fda186491f70e143
yunrui huang <yunruihuang@gmail.com>
2023-11-04 22:42:39 -0400

    11/4/2023 update notebook api

--- server/routers.js
+++ server/routers.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const dataPort = require('./router/dataPort');
+const note = require('./router/notebook');
 
 router.use("/data",dataPort);
+router.use("/note",note);
 
 module.exports=router;
