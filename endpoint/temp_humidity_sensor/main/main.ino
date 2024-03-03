0aff59f51f43cc2451c180f1f5a412b5b3786e59
YunRuiHuang <yunruihuang@gmail.com>
2024-03-03 17:58:46 -0500

    setup vue 3/3/2024

---
+++ endpoint/temp_humidity_sensor/main/main.ino
@@ -0,0 +1,138 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <DHT.h>
+
+const char* ssid="wifi name";
+const char* password="wifi password";
+const int machineId = 1;
+const String serverPath = "url";
+const int DHTPin = 14;
+const int brightPin = 12;
+int postCounter = 0;
+
+DHT dht(DHTPin,DHT11);
+
+void setup()
+{
+delay(1000);
+Serial.begin(115200);
+
+// pinMode(brightPin, INPUT);
+dht.begin();
+connectWifi();
+HttpGet(); 
+}
+
+void loop() {
+
+// int bright = analogRead(35);
+int bright = 0;
+
+float humidity = dht.readHumidity();
+float temperature = dht.readTemperature();
+if(isnan(humidity) || isnan(temperature)){
+  Serial.println("fail to read dht data");
+}else{  
+  Serial.print("humidity : ");
+  Serial.print(humidity);
+  Serial.print("  temp : ");
+  Serial.print(temperature); 
+  Serial.print("  bright : ");
+  Serial.println(bright); 
+  HttpPost(machineId,temperature,humidity,bright);
+}
+  
+
+for(int i =0; i< 10; i++){
+  delay(60000);
+}
+
+}
+
+
+//connect WIFI function
+void connectWifi(){
+  WiFi.begin(ssid, password);             // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  int counter = 0;
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(100);
+    Serial.print('>');
+    counter++;
+    if(counter > 100){
+      return;
+    }
+  }
+
+  Serial.println("\n =======");
+  Serial.println("Connection established!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  
+}
+
+
+void HttpGet(){
+  while(WiFi.status()!=WL_CONNECTED){
+    connectWifi();
+  }
+  HTTPClient http;
+  String targetPath = serverPath + "1";
+  
+  // Your Domain name with URL path or IP address with path
+  http.begin(targetPath.c_str());
+      
+  // Send HTTP GET request
+  int httpResponseCode = http.GET();
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    String payload = http.getString();
+    Serial.println(payload);
+  }else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+}
+
+void HttpPost(int machineId,float temp,float humidity,int bright){
+  while(WiFi.status()!=WL_CONNECTED){
+    connectWifi();
+  }
+  
+  postCounter++;
+  if(postCounter > 50){
+    ESP.restart();
+  }
+
+  HTTPClient http;
+  WiFiClient client;
+  String targetPath = serverPath + machineId;
+  
+  // Your Domain name with URL path or IP address with path
+  http.begin(client,targetPath.c_str());
+
+  // Specify content-type header  
+  http.addHeader("Content-Type", "application/json");  
+
+  String httpRequestData = "";
+  httpRequestData = httpRequestData + "{\"temp\":\"" + temp;
+  httpRequestData = httpRequestData + "\",\"humidity\":\"" + humidity;
+  httpRequestData = httpRequestData + "\",\"bright\":\"" + bright + "\"}";
+  Serial.println(httpRequestData);
+
+  // Send HTTP GET request
+  int httpResponseCode = http.POST(httpRequestData);
+  
+  Serial.print("HTTP Response code: ");
+  Serial.println(httpResponseCode);
+  String payload = http.getString();
+  Serial.println(payload);
+
+  // Free resources
+  http.end();
+}
