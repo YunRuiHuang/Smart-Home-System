d5dcf072f1020519d7b0141cc7a5d060432a82a3
YunRuiHuang <yunruihuang@gmail.com>
2024-03-06 19:40:20 -0500

    update 03/06/2024

--- smart-home frontend/src/App.vue
+++ smart-home frontend/src/App.vue
@@ -1,47 +1,158 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="f">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="header">
+      <p id="title" class="title">title</p>
     </div>
-  </header>
 
-  <main>
+    <div class="center">
+      
+      <div class="navigate">
+        <div class="navigate-control">
+          <img src="components/icons/X.jpg" alt="" style="height: 30px; width: 30px;">
+        </div>
+
+        <a href="index.html">
+          <div class="navigate-label">
+              <p>Home</p>
+          </div>
+        </a> 
+
+        <a href="Music.html">
+          <div class="navigate-label">
+              <p>Music</p>
+          </div>
+        </a> 
+
+        <a href="HomeStatus.html">
+          <div class="navigate-label">
+              <p>Home Status</p>
+          </div>
+        </a> 
+
+      </div>
+
+      <div class="page">
+        <!-- TODO -->
+      </div>
+
+    </div>
+
+
+    <div class="footer">
+      <a href="index.html">| Homepage |</a>
+      <br>
+      <a href="#top">back to top</a>
+    </div>
+
+
+  </div>
+
+
+  <!-- <main>
     <TheWelcome />
-  </main>
+  </main> -->
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+html, body{
+  height: 100%;
+  margin: 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.f{
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  height: 100%;
+  width: 100%;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+.header{
+  text-align: center;
+  width: 100%;
+  height: 8%;
+  border: 0;
+  border-bottom: 2px;
+  border-style: solid;
+}
+
+.center{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  height: 90%;
+  width: 100%;
+  border: 0;
+  border-bottom: 2px;
+  border-style: solid;
+}
+
+.navigate{
+  text-align: center;
+  height: 100%;
+  width: 15%;
+  border: 0;
+  border-right: 2px;
+  border-style: solid;
+}
+
+.navigate-control{
+  text-align: right;
+  border: 0;
+  border-bottom: 2px;
+  border-style: solid;
+}
+
+.navigate-label{
+  text-align: center;
+  height: 50px;
+  width: 100%;
+  border: 0;
+  border-bottom: 2px;
+  border-style: solid;
+}
+
+.page{
+  text-align: center;
+  height: 100%;
+  width: 85%;
+}
+
+.footer{
+  text-align: center;
+  height: auto;
+  width: 100%;
+}
+
+
+@media(max-width:600px) {
+  .center{
+    flex-direction: column;
+    overflow: scroll;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  .navigate{
+    text-align: center;
+    height: auto;
+    width: 100%;
+    border: 0;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .navigate-label{
+    height: 100px;
   }
+
+  .page{
+    height: auto;
+    width: 100%;
+  }
+
+
 }
 </style>
